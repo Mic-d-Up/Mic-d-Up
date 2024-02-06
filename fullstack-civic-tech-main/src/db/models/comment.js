48c4fa4e4db9f2d4eaa807b680e3ef6b9d4e6c89
petthecat19 <adrianpena365@gmail.com>
2024-02-06 15:59:41 -0500

    Started the creation of the comment model

---
+++ fullstack-civic-tech-main/src/db/models/comment.js
@@ -0,0 +1,66 @@
+const knex = require('../knex');
+const { hashPassword, isValidPassword } = require('../../utils/auth-utils');
+
+class Comment {
+  #passwordHash = null; 
+
+  constructor({ id, username, password_hash }) {
+    this.id = id;
+    this.username = username;
+    this.#passwordHash = password_hash;
+  }
+
+  static async list() {
+    const query = 'SELECT * FROM users';
+    const { rows } = await knex.raw(query);
+    // use the constructor to hide each user's passwordHash
+    return rows.map((user) => new User(user));
+  }
+
+  static async find(id) {
+    const query = 'SELECT * FROM users WHERE id = ?';
+    const args = [id];
+    const { rows } = await knex.raw(query, args);
+    const user = rows[0];
+    return user ? new User(user) : null;
+  }
+
+  static async findByUsername(username) {
+    const query = 'SELECT * FROM users WHERE username = ?';
+    const args = [username];
+    const { rows } = await knex.raw(query, args);
+    const user = rows[0];
+    return user ? new User(user) : null;
+  }
+
+  static async create(username, password) {
+    const passwordHash = await hashPassword(password);
+
+    const query = `INSERT INTO users (username, password_hash)
+      VALUES (?, ?) RETURNING *`;
+    const args = [username, passwordHash];
+    const { rows } = await knex.raw(query, args);
+    const user = rows[0];
+    return new User(user);
+  }
+
+  static async deleteAll() {
+    return knex.raw('TRUNCATE users;');
+  }
+
+  update = async (username) => { // dynamic queries are easier if you add more properties
+    const rows = await knex('users')
+      .where({ id: this.id })
+      .update({ username })
+      .returning('*');
+
+    const updatedUser = rows[0];
+    return updatedUser ? new User(updatedUser) : null;
+  };
+
+  isValidPassword = async (password) => (
+    isValidPassword(password, this.#passwordHash)
+  );
+}
+
+module.exports = User;

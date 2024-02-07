5c93729589d3fc896fd6983386174aa9985b92e2
PresTroph <prestigeguedegbe@gmail.com>
2024-02-07 15:36:06 -0500

    some-updates

--- fullstack-civic-tech-main/frontend/src/pages/Hub.jsx
+++
@@ -1,6 +0,0 @@
-export default function Hub() {
-  return <>
-    <h1>The Hub</h1>
-    <p>Put something interesting here!</p>
-</>;
-}
---
+++ fullstack-civic-tech-main/frontend/src/pages/HubTests.jsx
@@ -0,0 +1,22 @@
+// export default function Hub() {
+//   return <>
+//     <h1>The Hub</h1>
+//     <p>Put something interesting here!</p>
+// </>;
+// }
+
+// export default function HomePage() {
+//     return <>
+//       <h1>Home</h1>
+//       <p>Put something interesting here!</p>
+//     </>;
+//   }
+
+
+   { /* <h1 className="header">The Hub</h1>
+    <p>Experience our embracive Mic'd Up events,
+      where diverse voices unite and form lasting connections.
+      Convey your bonafide self while connecting with the collective in our welcoming spaces.</p>
+      <form className="events-form">
+      <div className="events-row"></div>
+      </form> */ }

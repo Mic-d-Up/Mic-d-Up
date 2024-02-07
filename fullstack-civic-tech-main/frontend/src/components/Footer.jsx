8ef8b9e73930f71beab23031bfe45d9d47bf0b8c
engineerErica <ericaponce2002@gmail.com>
2024-02-06 20:22:28 -0500

    footer component added to App.jsx

---
+++ fullstack-civic-tech-main/frontend/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+  return (
+    <footer class="footer">
+      <div class="content has-text-centered">
+        <p>
+        <strong>Mic'd Up</strong> by <a href="https://github.com/Mic-d-Up">Fearsome Four</a>. The source code is licensed.
+        <a href="https://github.com/Mic-d-Up/Mic-d-Up-Civic-Tech/tree/main/fullstack-civic-tech-main">Repository</a>. The website content
+      is licensed <a href="http://marcylabschool.org">Marcy Lab School</a>.
+      </p>
+    </div>
+  </footer>
+  )
+}

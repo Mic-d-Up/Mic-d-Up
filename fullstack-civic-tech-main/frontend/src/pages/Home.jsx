7bb9899932b5386fc8f00a535b3814c4bb9bb63e
petthecat19 <adrianpena365@gmail.com>
2024-02-07 16:01:00 -0500

    imported bulma into react hub page

--- fullstack-civic-tech-main/frontend/src/pages/Home.jsx
+++ fullstack-civic-tech-main/frontend/src/pages/Home.jsx
@@ -1,6 +1,11 @@
+import React from 'react';
+import 'bulma/css/bulma.min.css';
 export default function HomePage() {
-  return <>
+ 
+ 
+ return <>
     <h1>Home</h1>
     <p>Put something interesting here!</p>
   </>;
 }
+

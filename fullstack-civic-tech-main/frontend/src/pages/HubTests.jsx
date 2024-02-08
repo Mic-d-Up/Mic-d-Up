99ece15ba3b118c4c60e35941286242c295fbc0c
PresTroph <prestigeguedegbe@gmail.com>
2024-02-08 11:17:02 -0500

    file saving

--- fullstack-civic-tech-main/frontend/src/pages/HubTests.jsx
+++ fullstack-civic-tech-main/frontend/src/pages/HubTests.jsx
@@ -13,3 +13,33 @@
 //     </>;
 //   }
 
+
+// // eslint-disable-next-line no-unused-vars
+// import React from 'react';
+// import './HubHome.css';
+// import EventCard from '../components/EventCard';
+
+// const Hub = () => (
+//     <div className="hub">
+//       <h1>Hub</h1>
+//       <div className="event-container">
+//         <EventCard
+//           title="Event 1"
+//           imageSrc="/img/300.png"
+//           description="Description of Event 1"
+//         />
+//         <EventCard
+//           title="Event 2"
+//           imageSrc="/img/300.png"
+//           description="Description of Event 2"
+//         />
+//         <EventCard
+//           title="Event 3"
+//           imageSrc="/img/300.png"
+//           description="Description of Event 3"
+//         />
+//       </div>
+//     </div>
+// );
+
+// export default Hub;

924e36686dc78e3b11cf2e23579fdc09479c5305
engineerErica <ericaponce2002@gmail.com>
2024-02-08 12:08:04 -0500

    changed class to className on all the modal divs as well as moved the h3 and p tag inside the form

--- fullstack-civic-tech-main/frontend/src/components/CreateModal.jsx
+++ fullstack-civic-tech-main/frontend/src/components/CreateModal.jsx
@@ -1,68 +1,68 @@
 export default function CreateModal() {
   return <>
-    <div id="create-modal" class="modal">
-      <div class="modal-background"></div>
+    <div id="create-modal" className="modal">
+      <div className="modal-background"></div>
 
-      <div class="modal-content">
-        <div class="box">
-          <h3>What's going on?</h3>
-          <p>Tell us about the event</p>
+      <div className="modal-content">
+        <div className="box">
           <form>
-            <div class="field">
-              <label class="label">Name</label>
-              <div class="control">
-                <input class="input" type="text" placeholder="Name" />
+            <h3>What's going on?</h3>
+            <p>Tell us about the event</p>
+            <div className="field">
+              <label className="label">Name</label>
+              <div className="control">
+                <input className="input" type="text" placeholder="Name" />
               </div>
             </div>
 
-            <div class="field">
-              <label class="label">Date</label>
-              <div class="control">
-                <input class="input" type="date" placeholder="Date" value="bulma" />
+            <div className="field">
+              <label className="label">Date</label>
+              <div className="control">
+                <input className="input" type="date" placeholder="Date" value="bulma" />
               </div>
             </div>
 
-            <div class="field">
-              <label class="label">Start Time</label>
-              <div class="control">
-                <input class="input" type="time" placeholder="start time input" />
+            <div className="field">
+              <label className="label">Start Time</label>
+              <div className="control">
+                <input className="input" type="time" placeholder="start time input" />
               </div>
             </div>
 
-            <div class="field">
-              <label class="label">End Time</label>
-              <div class="control">
-                <input class="input" type="time" placeholder="end time input" />
+            <div className="field">
+              <label className="label">End Time</label>
+              <div className="control">
+                <input className="input" type="time" placeholder="end time input" />
               </div>
             </div>
 
-            <div class="field">
-              <label class="label">Location</label>
-              <div class="control">
-                <textarea class="textarea" placeholder="Textarea"></textarea>
+            <div className="field">
+              <label className="label">Location</label>
+              <div className="control">
+                <textarea className="textarea" placeholder="Textarea"></textarea>
               </div>
             </div>
 
-            <div class="field">
-              <label class="label">Tickets</label>
-              <div class="control">
-                <input class="input" type="text" />
+            <div className="field">
+              <label className="label">Tickets</label>
+              <div className="control">
+                <input className="input" type="text" />
               </div>
             </div>
 
-            <div class="field is-grouped">
-              <div class="control">
-                <button class="button is-link">Submit</button>
+            <div className="field is-grouped">
+              <div className="control">
+                <button className="button is-link">Submit</button>
               </div>
-              <div class="control">
-                <button class="button is-link is-light">Cancel</button>
+              <div className="control">
+                <button className="button is-link is-light">Cancel</button>
               </div>
             </div>
           </form>
         </div>
       </div>
 
-      <button class="modal-close is-large" aria-label="close"></button>
+      <button className="modal-close is-large" aria-label="close"></button>
     </div>
   </>
 }

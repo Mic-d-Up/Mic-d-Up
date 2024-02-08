5886845eb49e0a0c8fe4ff56fcb7d6cc0f9bab63
PresTroph <prestigeguedegbe@gmail.com>
2024-02-08 16:47:32 -0500

    even/comments work

--- fullstack-civic-tech-main/frontend/src/components/EventCard.jsx
+++ fullstack-civic-tech-main/frontend/src/components/EventCard.jsx
@@ -1,13 +1,34 @@
-export default function EventCard({ title, imageSrc, description }) {
+/* eslint-disable no-unused-vars */
+// eslint-disable-next-line max-len
+export default function EventCard({ user_id, location, name, date, start_time, end_time, ticket_link }) {
   return (
-    <>
-    <div className="event-card">
-      <img src={imageSrc} alt={title} className="event-image" />
-      <div className="event-details">
-        <h3>{title}</h3>
-        <p>{description}</p>
+    <><>
+      <div className="card">
+        <div className="card-content">
+          <div className="content">
+            <h4 className="name">Event Name</h4>
+            <h4 className="location">Location</h4>
+            <p>Start Time:</p>
+            <p>End Time:</p>
+            <p>Ticket Link</p>
+          </div>
+        </div>
       </div>
+    </>
+    <>
+    <div className="media-content">
+        <p className="title is-4">John Smith</p>
+        <p className="subtitle is-6">@johnsmith</p>
+      </div>
+
+    <div className="content">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+      <a href="#">#css</a> <a href="#">#responsive</a>
+      <br/>
+      <time dateTime="2016-1-1">11:09 PM - 1 Jan 2016</time>
     </div>
-         </>
+    </></>
+
   );
 }
--- fullstack-civic-tech-main/frontend/src/components/Footer.jsx
+++ fullstack-civic-tech-main/frontend/src/components/Footer.jsx
@@ -1,7 +1,8 @@
+/* eslint-disable react/no-unescaped-entities */
 export default function Footer() {
   return (
-    <footer class="footer">
-      <div class="content has-text-centered">
+    <footer className="footer">
+      <div className="content has-text-centered">
         <p>
         <strong>Mic'd Up</strong> by <a href="https://github.com/Mic-d-Up">Fearsome Four</a>. The source code is licensed.
         <a href="https://github.com/Mic-d-Up/Mic-d-Up-Civic-Tech/tree/main/fullstack-civic-tech-main">Repository</a>. The website content
@@ -9,5 +10,5 @@ export default function Footer() {
       </p>
     </div>
   </footer>
-  )
+  );
 }

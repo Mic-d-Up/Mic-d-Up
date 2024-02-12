728232355781c08cd5a76fe63c35d9afa903d396
engineerErica <ericaponce2002@gmail.com>
2024-02-12 13:32:16 -0500

    added check to see if the user was logged in before allowing the modal to create a new event to be displayed

--- fullstack-civic-tech-main/frontend/src/pages/Home.jsx
+++ fullstack-civic-tech-main/frontend/src/pages/Home.jsx
@@ -1,13 +1,22 @@
 import CreateModal from "../components/CreateModal";
 import { useState } from "react";
+import { useContext } from "react";
+import CurrentUserContext from "../contexts/current-user-context";
+import { useNavigate } from "react-router-dom";
 
 export default function HomePage() {
   const [showModal, setShowModal] = useState(false);
+  const { currentUser } = useContext(CurrentUserContext);
+  const navigate = useNavigate();
 
   return <>
     <h1>The Hub</h1>
     <p>Check out events from our community!</p>
-    <button type="button" onClick={() => setShowModal(!showModal)}>New Event</button>
+    {
+      !currentUser
+      ? <button type="button" onClick={() => navigate('/login')}>New Event</button>
+      : <button type="button" onClick={() => setShowModal(!showModal)}>New Event</button> 
+    }
     {showModal && <CreateModal setShowModal={setShowModal} onClose={() => setShowModal(false)} />}
   </>;
 }

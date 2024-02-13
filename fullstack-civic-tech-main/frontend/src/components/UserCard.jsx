2afb3207aa7667822c7a9a090d05d14cb6f51ce1
petthecat19 <adrianpena365@gmail.com>
2024-02-13 12:50:18 -0500

    removed bio feature

--- fullstack-civic-tech-main/frontend/src/components/UserCard.jsx
+++ fullstack-civic-tech-main/frontend/src/components/UserCard.jsx
@@ -15,14 +15,6 @@
         <p className="subtitle is-6">{artist_type}</p>
       </div>
     </div>
-
-    <div className="content">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-      Phasellus nec iaculis mauris. <a>@bulmaio</a>.
-      <a href="#">#css</a> <a href="#">#responsive</a>
-      <br />
-      <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
-    </div>
   </div>
 </div>
 </>

840190c95d80f370495144b8270355a6d2ab928d
engineerErica <ericaponce2002@gmail.com>
2024-02-21 14:55:03 -0500

    added user path to create comment route

--- fullstack-civic-tech-main/src/router.js
+++ fullstack-civic-tech-main/src/router.js
@@ -11,11 +11,11 @@ Router.get('/users', userController.list);
 Router.post('/users', userController.create);
 Router.get('/users/:id', userController.show);
 
-Router.get('/', eventController.list);
-Router.post('/', eventController.create);
+Router.get('/events', eventController.list);
+Router.post('/users/:id/events', eventController.create);
 
 Router.get('/events/:id/comments', commentController.list);
-Router.post('/events/:id/comments', commentController.create);
+Router.post('/events/:id/users/:id/comments', commentController.create);
 
 Router.post('/login', userController.login);
 Router.delete('/logout', userController.logout);

8d39540b69220f4be960c8fdf9058b47d3564678
Mayaluisa <mayaluisa.skynetwork@gmail.com>
2024-02-22 12:39:35 -0500

    changes to footer.

--- fullstack-civic-tech-main/frontend/src/components/Footer.jsx
+++ fullstack-civic-tech-main/frontend/src/components/Footer.jsx
@@ -3,9 +3,7 @@ export default function Footer() {
     <footer className="footer">
       <div className="content has-text-centered">
         <p>
-        by <a href="https://github.com/Mic-d-Up">Fearsome Four</a>. The source code is licensed.
-        <a href="https://github.com/Mic-d-Up/Mic-d-Up-Civic-Tech/tree/main/fullstack-civic-tech-main">Repository</a>. The website content
-      is licensed <a href="http://marcylabschool.org">Marcy Lab School</a>.
+        
       </p>
     </div>
   </footer>

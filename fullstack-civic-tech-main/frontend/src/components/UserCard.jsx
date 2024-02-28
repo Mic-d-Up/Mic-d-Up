f798644bd97a55181913f9ec823a0bd7d8380232
petthecat19 <adrianpena365@gmail.com>
2024-02-28 12:58:02 -0500

    ran npm run build to send my front end changes to the back end

--- fullstack-civic-tech-main/frontend/src/components/UserCard.jsx
+++ fullstack-civic-tech-main/frontend/src/components/UserCard.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import DefaultImageUrl from "../img/default-profile-picture.png";
+// import "../"
 
 export default function UserCard({ user }) {
   return (

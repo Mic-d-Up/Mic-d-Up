38004cf2357185e07fab0f98a1816f62cd332cc5
PresTroph <prestigeguedegbe@gmail.com>
2024-02-27 22:59:05 -0500

    home page styling more work to be done

--- fullstack-civic-tech-main/frontend/src/components/EventCard.jsx
+++ fullstack-civic-tech-main/frontend/src/components/EventCard.jsx
@@ -1,14 +1,15 @@
+/* eslint-disable import/order */
 /* eslint-disable no-shadow */
 // eslint-disable-next-line no-unused-vars
 import React, { useState, useContext } from 'react';
 import { userJoinEvent, userLeaveEvent } from '../adapters/event-adapter';
 import CurrentUserContext from "../contexts/current-user-context";
-import "./button.css" 
+import "./button.css";
 import { createComment, getAllComments } from '../adapters/comment-adapter';
-
+import { Button, Card } from 'react-bootstrap';
 
 const EventCard = (props) => {
-  const {event, joinedEvents, loadJoinEvents} = props
+  const { event, joinedEvents, loadJoinEvents } = props;
   const [userInput, setUserInput] = useState('');
   const [comments, setComments] = useState([]);
   const { currentUser } = useContext(CurrentUserContext);
@@ -16,65 +17,65 @@ const EventCard = (props) => {
   const handleCommentChange = (e) => {
     setUserInput(e.target.value);
   };
-  
+
   const handleCommentSubmit = async (e) => {
     e.preventDefault();
     const user_id = currentUser.id;
     if (userInput.trim() !== '') {
       setComments([...comments, userInput]);
       const content = userInput;
-      const [comment, error] = await createComment( { user_id, event_id, content });
+      const [comment, error] = await createComment({ user_id, event_id, content });
       setUserInput('');
     }
   };
 
   const joinEvent = async () => {
-    const row = await userJoinEvent(currentUser.id, event.id)
+    const row = await userJoinEvent(currentUser.id, event.id);
     setTimeout(async () => {
-      console.log("ran")
+      console.log("ran");
       await loadJoinEvents();
     }, 50);
   };
 
   const leaveEvent = async () => {
-    const row = await userLeaveEvent(currentUser.id, event.id)
+    const row = await userLeaveEvent(currentUser.id, event.id);
     setTimeout(async () => {
-      console.log("ran")
+      console.log("ran");
       await loadJoinEvents();
     }, 50);
   };
 
-
   return (
-    <div className="card">
-      <div className="card-content">
-        <p className="title">{event.name}</p>
-        <p>{event.location}</p>
-        <p>Date: {event.date}</p>
-        <p>Time: {event.startTime} - {event.endTime}</p>
-        <a href={event.ticketLink} target="_blank" rel="noopener noreferrer">Get Tickets</a>
-        { joinedEvents && joinedEvents[event.id] ? <button onClick={leaveEvent} className={joinedEvents[event.id] ? 'leave-event' : 'join-event'}>Leave Event</button> : <button className={joinedEvents[event.id] ? 'leave-event' : 'join-event'} onClick={joinEvent}>Join Event</button>}
-      </div>
-      <footer className="card-footer">
-        <div className="card-footer-item">
-          <form onSubmit={handleCommentSubmit}>
-            <div className="field has-addons">
-              <div className="control">
-                <input
-                  className="input"
-                  type="text"
-                  placeholder="Add a comment..."
-                  value={userInput}
-                  onChange={handleCommentChange}
-                />
-              </div>
-              <div className="control">
-                <button type="submit" className="button is-primary">Comment</button>
-              </div>
-            </div>
-          </form>
+
+    <Card className="event-card">
+    <Card.Body>
+      <Card.Title>{event.name}</Card.Title>
+      <Card.Text>
+        {event.location}
+        <br />
+        Date: {event.date}
+        <br />
+        Time: {event.startTime} - {event.endTime}
+      </Card.Text>
+      <Button variant="primary" href={event.ticketLink} target="_blank" rel="noopener noreferrer">Get Tickets</Button>
+      {joinedEvents && joinedEvents[event.id] ? 
+        <Button onClick={leaveEvent} variant="secondary">Leave Event</Button> :
+        <Button onClick={joinEvent} variant="secondary">Join Event</Button>
+      }
+    </Card.Body>
+    <Card.Footer>
+      <form onSubmit={handleCommentSubmit}>
+        <div className="form-group">
+          <input
+            className="form-control"
+            type="text"
+            placeholder="Add a comment..."
+            value={userInput}
+            onChange={handleCommentChange}
+          />
         </div>
-      </footer>
+        <Button type="submit" variant="primary">Comment</Button>
+      </form>
       <div className="card-content">
         <div className="content">
           {comments.map((comment, index) => (
@@ -82,7 +83,9 @@ const EventCard = (props) => {
           ))}
         </div>
       </div>
-    </div>
+    </Card.Footer>
+  </Card>
+
   );
 };
 
--- fullstack-civic-tech-main/frontend/src/pages/Home.jsx
+++ fullstack-civic-tech-main/frontend/src/pages/Home.jsx
@@ -1,12 +1,74 @@
 /* eslint-disable max-len */
 /* eslint-disable no-unused-vars */
+// import { useState, useContext, useEffect } from "react";
+// import { useNavigate } from "react-router-dom";
+// import { getAllEvents, fetchAllJoinedEvents } from "../adapters/event-adapter";
+// import CurrentUserContext from "../contexts/current-user-context";
+// import CreateModal from "../components/CreateModal";
+// import EventCard from '../components/EventCard';
+// import { Carousel } from 'react-bootstrap';
+
+// export default function HomePage() {
+//   const { currentUser } = useContext(CurrentUserContext);
+//   const [showModal, setShowModal] = useState(false);
+//   const [events, setEvents] = useState([]);
+//   const [joinedEvents, setJoinedEvents] = useState({});
+//   const getEveryEvent = async () => {
+//     const eventsObj = await getAllEvents();
+//     console.log(eventsObj);
+//     setEvents(eventsObj);
+//   };
+//   useEffect(() => {
+//     getEveryEvent();
+//   }, []);
+
+//   const navigate = useNavigate();
+
+//   const loadJoinEvents = async () => {
+//     console.log("guh");
+//     if (currentUser) {
+//       const attendedEvents = await fetchAllJoinedEvents(currentUser.id);
+//       console.log(attendedEvents);
+//       const obj = {};
+//       attendedEvents.map((event) => {
+//         console.log("should be event");
+//         console.log(event);
+//         obj[event.event_id] = true;
+//       });
+//       setJoinedEvents(obj);
+//     }
+
+//   };
+
+//   useEffect(() => {
+//     loadJoinEvents();
+//   }, [currentUser]);
+
+//   return <>
+//     <h1>The Hub</h1>
+//     <p>Check out events from our community!</p>
+//     <button onClick={loadJoinEvents}>Test</button>
+//     {
+//       !currentUser
+//         ? <button type="button" onClick={() => navigate('/login')}>New Event</button>
+//         : <button type="button" onClick={() => setShowModal(!showModal)}>New Event</button>
+//     }
+//     {events.map((event) => (<li key={event.id}>
+//         <EventCard event={event} loadJoinEvents={loadJoinEvents} joinedEvents={joinedEvents} />
+//       </li>))}
+//     {console.log(joinedEvents)}
+//     {showModal && <CreateModal getEveryEvent= {getEveryEvent} setShowModal={setShowModal} onClose={() => setShowModal(false)} />}
+//   </>;
+// }
+
 import { useState, useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { Carousel, Row, Col } from 'react-bootstrap';
 import { getAllEvents, fetchAllJoinedEvents } from "../adapters/event-adapter";
 import CurrentUserContext from "../contexts/current-user-context";
 import CreateModal from "../components/CreateModal";
 import EventCard from '../components/EventCard';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function HomePage() {
   const { currentUser } = useContext(CurrentUserContext);
@@ -15,52 +77,62 @@ export default function HomePage() {
   const [joinedEvents, setJoinedEvents] = useState({});
   const getEveryEvent = async () => {
     const eventsObj = await getAllEvents();
-    console.log(eventsObj);
     setEvents(eventsObj);
   };
+
   useEffect(() => {
     getEveryEvent();
   }, []);
 
- 
   const navigate = useNavigate();
 
   const loadJoinEvents = async () => {
-    console.log("guh");
     if (currentUser) {
       const attendedEvents = await fetchAllJoinedEvents(currentUser.id);
-      console.log(attendedEvents);
       const obj = {};
-      attendedEvents.map((event) => {
-        console.log("should be event");
-        console.log(event);
+      attendedEvents.forEach((event) => {
         obj[event.event_id] = true;
       });
       setJoinedEvents(obj);
     }
-
-
   };
 
   useEffect(() => {
     loadJoinEvents();
   }, [currentUser]);
 
-
-
-  return <>
-    <h1>The Hub</h1>
-    <p>Check out events from our community!</p>
-    <button onClick={loadJoinEvents}>Test</button>
-    {
-      !currentUser
-        ? <button type="button" onClick={() => navigate('/login')}>New Event</button>
-        : <button type="button" onClick={() => setShowModal(!showModal)}>New Event</button>
+  return (
+    <div className="carousel-container">
+  <h1>The Hub</h1>
+  <p>Check out events from our community!</p>
+  <div className="cs">
+    {/* Conditional rendering for 'New Event' button based on currentUser */}
+    {!currentUser
+      ? <button type="button" onClick={() => navigate('/login')}>New Event</button>
+      : <button type="button" onClick={() => setShowModal(!showModal)}>New Event</button>
     }
-    {events.map((event) => (<li key={event.id}>
-        <EventCard event={event} loadJoinEvents={loadJoinEvents} joinedEvents={joinedEvents} />
-      </li>))}
-    {console.log(joinedEvents)}
-    {showModal && <CreateModal getEveryEvent= {getEveryEvent} setShowModal={setShowModal} onClose={() => setShowModal(false)} />}
-  </>;
+    {/* Carousel component */}
+    <Carousel interval={null} className="custom-carousel">
+      {/* Render multiple cards within each Carousel.Item */}
+      {events.map((event, index) => (
+        index % 4 === 0 && (
+          <Carousel.Item key={index}>
+            {/* Center the content horizontally */}
+            <div className="row justify-content-center">
+              {events.slice(index, index + 3).map((event, subIndex) => (
+                <div key={subIndex} className="col-md-3 mb-3">
+                  <EventCard event={event} loadJoinEvents={loadJoinEvents} joinedEvents={joinedEvents} />
+                </div>
+              ))}
+            </div>
+          </Carousel.Item>
+        )
+      ))}
+    </Carousel>
+  </div>
+  {/* Modal component */}
+  {showModal && <CreateModal getEveryEvent={getEveryEvent} setShowModal={setShowModal} onClose={() => setShowModal(false)} />}
+</div>
+
+  );
 }

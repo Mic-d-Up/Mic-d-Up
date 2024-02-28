ce6369bb8de3da218400aac5dde2527acf4f4793
PresTroph <prestigeguedegbe@gmail.com>
2024-02-28 17:00:39 -0500

    styling almost complete majority of styling is done and few things needed remaining.

--- fullstack-civic-tech-main/frontend/src/components/EventCard.jsx
+++ fullstack-civic-tech-main/frontend/src/components/EventCard.jsx
@@ -4,10 +4,11 @@
 import React, { useState, useContext } from 'react';
 import { userJoinEvent, userLeaveEvent } from '../adapters/event-adapter';
 import CurrentUserContext from "../contexts/current-user-context";
-import "./button.css"
+import "./button.css";
 import { createComment, getAllComments } from '../adapters/comment-adapter';
+
 const EventCard = (props) => {
-  const {event, joinedEvents, loadJoinEvents} = props
+  const { event, joinedEvents, loadJoinEvents } = props;
   const [userInput, setUserInput] = useState('');
   const [comments, setComments] = useState([]);
   const { currentUser } = useContext(CurrentUserContext);
@@ -21,62 +22,64 @@ const EventCard = (props) => {
     if (userInput.trim() !== '') {
       setComments([...comments, userInput]);
       const content = userInput;
-      const [comment, error] = await createComment( { user_id, event_id, content });
+      const [comment, error] = await createComment({ user_id, event_id, content });
       setUserInput('');
     }
   };
   const joinEvent = async () => {
-    const row = await userJoinEvent(currentUser.id, event.id)
+    const row = await userJoinEvent(currentUser.id, event.id);
     setTimeout(async () => {
-      console.log("ran")
+      console.log("ran");
       await loadJoinEvents();
     }, 50);
   };
   const leaveEvent = async () => {
-    const row = await userLeaveEvent(currentUser.id, event.id)
+    const row = await userLeaveEvent(currentUser.id, event.id);
     setTimeout(async () => {
-      console.log("ran")
+      console.log("ran");
       await loadJoinEvents();
     }, 50);
   };
   return (
-    <div className="card">
-      <div className="card-content">
-        <p className="title">{event.name}</p>
-        <p>{event.location}</p>
-        <p>Date: {event.date}</p>
-        <p>Time: {event.startTime} - {event.endTime}</p>
-        <a href={event.ticketLink} target="_blank" rel="noopener noreferrer">Get Tickets</a>
-        { joinedEvents && joinedEvents[event.id] ? <button onClick={leaveEvent} className={joinedEvents[event.id] ? 'leave-event' : 'join-event'}>Leave Event</button> : <button className={joinedEvents[event.id] ? 'leave-event' : 'join-event'} onClick={joinEvent}>Join Event</button>}
-      </div>
-      <footer className="card-footer">
-        <div className="card-footer-item">
-          <form onSubmit={handleCommentSubmit}>
-            <div className="field has-addons">
-              <div className="control">
-                <input
-                  className="input"
-                  type="text"
-                  placeholder="Add a comment..."
-                  value={userInput}
-                  onChange={handleCommentChange}
-                />
-              </div>
-              <div className="control">
-                <button type="submit" className="button is-primary">Comment</button>
-              </div>
-            </div>
-          </form>
-        </div>
-      </footer>
-      <div className="card-content">
-        <div className="content">
-          {comments.map((comment, index) => (
-            <p key={index}>{comment}</p>
-          ))}
-        </div>
+    <div className="flex flex-col card shadow-md rounded-lg overflow-hidden max-w-xs mx-auto">
+    <div className="p-4 flex flex-col gap-3">
+      <p className="text-lg font-bold text-center">{event.name}</p>
+      <p className="text-sm  font-semibold">Location: {event.location}</p>
+      <p className="text-sm font-semibold">Date: {event.date}</p>
+      <p className="text-sm font-semibold">Time: {event.startTime} - {event.endTime}</p>
+      <button href={event.ticketLink} target="_blank" rel="noopener noreferrer" className="inline-block mt-2 text-blue-500 hover:text-blue-600">Get Tickets</button>
+      {joinedEvents && joinedEvents[event.id] ? (
+        <button onClick={leaveEvent} className={`mt-4 inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white ${joinedEvents[event.id] ? 'bg-red-600 hover:bg-red-700' : 'bg-blue-600 hover:bg-blue-700'}`}>
+          Leave Event
+        </button>
+      ) : (
+        <button onClick={joinEvent} className="mt-4 inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700">
+          Join Event
+        </button>
+      )}
+    </div>
+    <div className="px-4 py-3 bg-gray-50 text-right ">
+      <form onSubmit={handleCommentSubmit} className="flex items-center gap-10">
+        <input
+          className="flex-1 py-2 px-4 text-sm  text-gray-700 bg-white  rounded shadow focus:outline-none focus:shadow-outline"
+          type="text"
+          placeholder="Add a comment..."
+          value={userInput}
+          onChange={handleCommentChange}
+        />
+        <button type="submit" className="inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-green-600 hover:bg-green-700">
+          Comment
+        </button>
+      </form>
+    </div>
+    <div className="p-4">
+      <div className="space-y-2">
+        {comments.map((comment, index) => (
+          <p key={index} className="text-sm text-gray-600">{comment}</p>
+        ))}
       </div>
     </div>
-  );
+  </div>
+);
 };
 export default EventCard;
--- fullstack-civic-tech-main/frontend/src/pages/Home.jsx
+++ fullstack-civic-tech-main/frontend/src/pages/Home.jsx
@@ -1,12 +1,18 @@
 /* eslint-disable max-len */
 /* eslint-disable no-unused-vars */
+
 // import { useState, useContext, useEffect } from "react";
 // import { useNavigate } from "react-router-dom";
+// import SwiperCore, { Navigation, Pagination, Autoplay } from 'swiper';
+// import { Swiper, SwiperSlide } from 'swiper/react';
+// // import { Carousel } from "../components/Carousel";
 // import { getAllEvents, fetchAllJoinedEvents } from "../adapters/event-adapter";
 // import CurrentUserContext from "../contexts/current-user-context";
 // import CreateModal from "../components/CreateModal";
 // import EventCard from '../components/EventCard';
-// import { Carousel } from 'react-bootstrap';
+// import 'swiper/swiper-bundle.css';
+
+// SwiperCore.use([Navigation, Pagination, Autoplay]);
 
 // export default function HomePage() {
 //   const { currentUser } = useContext(CurrentUserContext);
@@ -21,9 +27,7 @@
 //   useEffect(() => {
 //     getEveryEvent();
 //   }, []);
-
 //   const navigate = useNavigate();
-
 //   const loadJoinEvents = async () => {
 //     console.log("guh");
 //     if (currentUser) {
@@ -37,7 +41,6 @@
 //       });
 //       setJoinedEvents(obj);
 //     }
-
 //   };
 
 //   useEffect(() => {
@@ -61,78 +64,71 @@
 //   </>;
 // }
 
-import { useState, useContext, useEffect } from "react";
+
+
+
+
+import React, { useState, useEffect, useContext } from 'react';
 import { useNavigate } from "react-router-dom";
-import { Carousel, Row, Col } from 'react-bootstrap';
-import { getAllEvents, fetchAllJoinedEvents } from "../adapters/event-adapter";
+import EventCarousel from '../components/EventCarousel';
 import CurrentUserContext from "../contexts/current-user-context";
 import CreateModal from "../components/CreateModal";
-import EventCard from '../components/EventCard';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { getAllEvents, fetchAllJoinedEvents } from '../adapters/event-adapter';
 
 export default function HomePage() {
   const { currentUser } = useContext(CurrentUserContext);
   const [showModal, setShowModal] = useState(false);
   const [events, setEvents] = useState([]);
   const [joinedEvents, setJoinedEvents] = useState({});
+  
+  // Function to fetch all events
   const getEveryEvent = async () => {
     const eventsObj = await getAllEvents();
     setEvents(eventsObj);
   };
 
+  // Fetch events on initial render
   useEffect(() => {
     getEveryEvent();
   }, []);
 
-  const navigate = useNavigate();
-
+  // Function to load joined events
   const loadJoinEvents = async () => {
     if (currentUser) {
       const attendedEvents = await fetchAllJoinedEvents(currentUser.id);
       const obj = {};
-      attendedEvents.forEach((event) => {
+      attendedEvents.forEach(event => {
         obj[event.event_id] = true;
       });
       setJoinedEvents(obj);
     }
   };
 
+  // Load joined events when currentUser changes
   useEffect(() => {
     loadJoinEvents();
   }, [currentUser]);
 
-  return (
-    <div className="carousel-container">
-  <h1>The Hub</h1>
-  <p>Check out events from our community!</p>
-  <div className="cs">
-    {/* Conditional rendering for 'New Event' button based on currentUser */}
-    {!currentUser
-      ? <button type="button" onClick={() => navigate('/login')}>New Event</button>
-      : <button type="button" onClick={() => setShowModal(!showModal)}>New Event</button>
-    }
-    {/* Carousel component */}
-    <Carousel interval={null} className="custom-carousel">
-      {/* Render multiple cards within each Carousel.Item */}
-      {events.map((event, index) => (
-        index % 4 === 0 && (
-          <Carousel.Item key={index}>
-            {/* Center the content horizontally */}
-            <div className="row justify-content-center">
-              {events.slice(index, index + 3).map((event, subIndex) => (
-                <div key={subIndex} className="col-md-3 mb-3">
-                  <EventCard event={event} loadJoinEvents={loadJoinEvents} joinedEvents={joinedEvents} />
-                </div>
-              ))}
-            </div>
-          </Carousel.Item>
-        )
-      ))}
-    </Carousel>
-  </div>
-  {/* Modal component */}
-  {showModal && <CreateModal getEveryEvent={getEveryEvent} setShowModal={setShowModal} onClose={() => setShowModal(false)} />}
-</div>
+  // Navigation hook for redirecting
+  const navigate = useNavigate();
 
+  return (
+    <div className=" p-4 h-[100vh] bg-[linear-gradient(180deg,rgba(255,255,255,1)_0%,rgba(136,3,212,1)_0%,rgba(0,0,0,1)_100%)]">
+    <div className=" flex flex-col gap-5 mb-20">
+    
+      <h1 className="text-2xl font-bold">The Hub</h1>  {(
+        <button
+          className="text-white  w-fit bg-[#211F22] hover:bg-[#FFFFFF] font-bold py-2 px-4 rounded"
+          onClick={() => setShowModal(true)}
+        >
+          New Event
+        </button>
+      )}
+    </div>
+      
+    <EventCarousel events={events} />
+    {showModal && <CreateModal setShowModal={setShowModal} />}
+    </div>
+ 
   );
 }
